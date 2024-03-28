202593422906423a1cdc97687b9ef5ab648981d3
Attoungbre Ange François <angeattoungbre21@gmail.com>
2024-03-28 15:22:30 +0000

    Utilisation de l'état de la variable pour afficher une sous section

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,47 @@
-import { useState } from 'react';
-import { CORE_CONCEPTS } from './data.js';
-import Header from './components/Header/Header.jsx';
-import CoreConcept from './components/CoreConcept.jsx';
-import TabButton from './components/TabButton/TabButton.jsx';
-function App() {
-  const selectedContent = "Please select a menu";
-  const [stateContent, setContent] = useState(selectedContent);
-  
+import { useState } from "react";
+import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton/TabButton.jsx";
+import { EXAMPLES } from "./data.js";
 
-  function handleClick(selectedButton){
+function App() {
+  const selectedContent = "components";
+  const [stateContent, setContent] = useState(selectedContent);
+
+  function handleClick(selectedButton) {
     setContent(selectedButton);
- }
+  }
   return (
     <div>
       <Header />
       <main>
         <h2>Core concepts</h2>
-        <section id='core-concepts'>
-        <ul>
-          <CoreConcept {...CORE_CONCEPTS[0]}/>
-          <CoreConcept {...CORE_CONCEPTS[1]}/>
-          <CoreConcept {...CORE_CONCEPTS[2]}/>
-          <CoreConcept {...CORE_CONCEPTS[3]}/>
-        </ul>
+        <section id="core-concepts">
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
         </section>
-        <section id='examples'>
+        <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleClick("components")}>Components</TabButton>
+            <TabButton onSelect={() => handleClick("components")}>
+              Components
+            </TabButton>
             <TabButton onSelect={() => handleClick("jsx")}>JSX</TabButton>
             <TabButton onSelect={() => handleClick("props")}>Props</TabButton>
             <TabButton onSelect={() => handleClick("state")}>State</TabButton>
           </menu>
-           {stateContent}
+          <div id="tab-content">
+            <h3>{EXAMPLES[stateContent].title}</h3>
+            <p>{EXAMPLES[stateContent].description}</p>
+            <pre>
+              <code>{EXAMPLES[stateContent].code}</code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
