81f05f26cbd9b8ee519590a07121ee3eb1dfc728
Attoungbre Ange François <angeattoungbre21@gmail.com>
2024-03-28 21:49:41 +0000

    Utilisation de map pour afficher de manière dynamique des éléments d'un tableau

--- src/App.jsx
+++ src/App.jsx
@@ -11,18 +11,20 @@ function App() {
   function handleClick(selectedButton) {
     setContent(selectedButton);
   }
-  function handleActive(value){
+  function handleActive(value) {
     return stateContent === value;
   }
   let tabContent = <p>Please select a topic !</p>;
-  if(stateContent){
-    tabContent = <div id="tab-content">
-            <h3>{EXAMPLES[stateContent].title}</h3>
-            <p>{EXAMPLES[stateContent].description}</p>
-            <pre>
-              <code>{EXAMPLES[stateContent].code}</code>
-            </pre>
-          </div>
+  if (stateContent) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[stateContent].title}</h3>
+        <p>{EXAMPLES[stateContent].description}</p>
+        <pre>
+          <code>{EXAMPLES[stateContent].code}</code>
+        </pre>
+      </div>
+    );
   }
 
   return (
@@ -32,29 +34,38 @@ function App() {
         <h2>Core concepts</h2>
         <section id="core-concepts">
           <ul>
-            <CoreConcept {...CORE_CONCEPTS[0]} />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} />
+            {CORE_CONCEPTS.map((item) => {
+              return <CoreConcept key="" {...item} />;
+            })}
           </ul>
         </section>
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton 
-            isSelected={handleActive("components")}
-            onSelect={() => handleClick("components")}>
+            <TabButton
+              isSelected={handleActive("components")}
+              onSelect={() => handleClick("components")}
+            >
               Components
             </TabButton>
             <TabButton
-             isSelected={handleActive("jsx")}
-             onSelect={() => handleClick("jsx")}>JSX</TabButton>
-            <TabButton 
-            isSelected={handleActive("props")}
-            onSelect={() => handleClick("props")}>Props</TabButton>
-            <TabButton 
-            isSelected={handleActive("state")}
-            onSelect={() => handleClick("state")}>State</TabButton>
+              isSelected={handleActive("jsx")}
+              onSelect={() => handleClick("jsx")}
+            >
+              JSX
+            </TabButton>
+            <TabButton
+              isSelected={handleActive("props")}
+              onSelect={() => handleClick("props")}
+            >
+              Props
+            </TabButton>
+            <TabButton
+              isSelected={handleActive("state")}
+              onSelect={() => handleClick("state")}
+            >
+              State
+            </TabButton>
           </menu>
           {tabContent}
         </section>
